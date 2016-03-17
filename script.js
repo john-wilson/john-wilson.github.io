6e84f5d5e3461757ccdd1695a07eaaa15286e7bf
john-wilson <irockwilson@gmail.com>
2016-03-17 00:09:20 -0700

    adding js file

---
+++ script.js
@@ -0,0 +1,35 @@
+var main = function() {
+
+    //========== START OF NAVBAR ============//
+    $('#navicon').click(function() {
+        $('.nav').css({
+            'transform': 'translateX(0)'
+        });
+    });
+
+    $('.nav').click(function() {
+        $('.nav').css({
+            'transform': 'translateX(-400px)'
+        });
+    });
+
+    
+
+    function _exit() {
+        $('.nav').css({
+            'transform': 'translateX(-200px)'
+        });
+    };
+    //========== END OF NAVBAR ============//
+    //========== START OF TO-TOP ============//
+    $('button').click(function() {
+        $('html, body').animate({
+            scrollTop: 0
+        }, "slow");
+    });
+    //========== END OF TO-TOP ============//
+
+
+};
+
+$(document).ready(main);
